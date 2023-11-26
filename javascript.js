1bb1ca65d05e101d864b19554521b87add501d35
IpatD <149626748+IpatD@users.noreply.github.com>
2023-11-26 15:24:54 +0100

    added pseudo loop to the game

--- javascript.js
+++ javascript.js
@@ -10,6 +10,7 @@ Scissors beat paper */
 3. match them
 4. show the score*/
 
+
 /* makes computer choice */
 function getComputerChoice (max = 3) {
     numComp = Math.floor(Math.random()* max);
@@ -26,38 +27,38 @@ function getPlayerChoice(){
     let choice = string.toLowerCase()
     return choice
 }
-const compSel = getComputerChoice()
-let playerSel = getPlayerChoice() 
-/* governs athe game and keeps score */
-function game () {
 
+function game(){
+    let playerSel = getPlayerChoice();
+    let compSel = getComputerChoice();
+    return playRound(playerSel, compSel);
 }
 
-
-/* Plays a round */
-function playRound () {
-    if (compSel == "paper" && playerSel == "rock")
-        
-        return "Computer wins!" 
-
-    else if (compSel == "paper" && playerSel == "scissors")
-        return "Player wins!"
-    else if (compSel == "paper" && playerSel == "paper")
-        return "Tie"
-    else if (compSel == "rock" && playerSel == "rock")
-        return "Tie"
-    else if (compSel == "rock" && playerSel == "paper")
-        return "Player Wins!"
-    else if (compSel == "rock" && playerSel == "scissors")
-        return "Computer wins!"
-    else if (compSel == "scissors" && playerSel == "rock")
-        return "Player Wins!"
-    else if (compSel == "scissors" && playerSel == "paper")
-        return "PComputer Wins!"
-    else if (compSel == "scissors" && playerSel == "scissors")
-        return "Tie!"
+function playRound (playerSel, compSel) {    
+    if (compSel == "paper" && playerSel == "rock") {
+        return "Computer wins!"}
+    else if (compSel == "paper" && playerSel == "scissors"){
+        return "Player wins!"}
+    else if (compSel == "paper" && playerSel == "paper"){
+        return "Tie"}
+    else if (compSel == "rock" && playerSel == "rock"){
+        return "Tie"}
+    else if (compSel == "rock" && playerSel == "paper"){
+        return "Player Wins!"}
+    else if (compSel == "rock" && playerSel == "scissors"){
+        return "Computer wins!"}
+    else if (compSel == "scissors" && playerSel == "rock"){
+        return "Player Wins!"}
+    else if (compSel == "scissors" && playerSel == "paper"){
+        return "Computer Wins!"}
+    else if (compSel == "scissors" && playerSel == "scissors"){
+        return "Tie!"}
     else
         return "no such weapon in the arsenal"
 }
 
-
+console.log(game())
+console.log(game())
+console.log(game())
+console.log(game())
+console.log(game())
